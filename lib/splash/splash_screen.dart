494c5e4212dadf5db1e9c51f441ac6559f5e8257
Ahmed Allam <ahmed.allam@izam.com>
2025-08-18 18:40:01 +0300

    Splash Screen

---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:depi_7_25/auth/auth_screen.dart';
+import 'package:depi_7_25/const.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  String image = "logo_2.svg";
+  Color backgroundColor = Colors.green;
+
+  Timer? timer;
+  int count = 0;
+
+  @override
+  initState() {
+    timer = Timer.periodic(const Duration(milliseconds: 500), ((timer) {
+      count++;
+      if (backgroundColor == Colors.green) {
+        image = "logo_3.svg";
+        backgroundColor = Colors.white;
+      } else {
+        image = "logo_2.svg";
+        backgroundColor = Colors.green;
+      }
+
+      setState(() {});
+      if (count == 5) {
+        Get.to(AuthScreen());
+      }
+    }));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(child: SvgPicture.asset(imagePath + image)),
+    );
+  }
+}
